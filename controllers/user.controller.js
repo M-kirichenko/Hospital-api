22d2a8b2b26f86bcc904360c23406daf8a13f54b
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-05-04 13:40:47 +0400

    db, token processes fixed

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -11,33 +11,34 @@ const {
 exports.register = async (req, res) => {
   const { body } = req;
   const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    res.status(422);
-    res.send({ msg: "All inputs required" });
-  } else {
-    const errors = [];
-    if (!validEmail(body["email"])) errors.push("Invalid email");
-    if (!validPassword(body["password"])) errors.push("Invalid password");
-    if (!validName(body["name"]))
-      errors.push(
-        "The name must be composed only of letters and be at least 3 letters long"
-      );
-    if (errors.length) {
-      res.status(422);
-      res.send({ msg: errors });
-    }
-    const existed = await user.findOne({ where: { email: body.email } });
-    if (existed) {
-      res.status(422);
-      res.send({ msg: "User with such mail already exists" });
-    }
-    jwt.sign(body, process.env.TOKEN_KEY, async (err, token) => {
-      body.password = await bcrypt.hash(body.password, 10);
-      const { email } = await user.create(body);
+  if (!name || !email || !password)
+    res.status(422).send({ msg: "All inputs required" });
+
+  const errors = [];
+
+  if (!validEmail(email)) errors.push("Invalid email");
+  if (!validPassword(password)) errors.push("Invalid password");
+  if (!validName(name))
+    errors.push(
+      "The name must be composed only of letters and be at least 3 letters long"
+    );
+  if (errors.length) res.status(422).send({ msg: errors });
+  const existed = await user.findOne({ where: { email: body.email } });
+  if (existed)
+    res.status(422).send({ msg: "User with such mail already exists" });
+
+  body.password = bcrypt.hashSync(body.password, 10);
+
+  user
+    .create(body)
+    .then((created) => {
+      const token = jwt.sign({ id: created.id, email }, process.env.TOKEN_KEY);
       res.send({
         email,
         token,
       });
+    })
+    .catch((err) => {
+      res.status(422).send({ msg: err });
     });
-  }
 };
--- index.js
+++ index.js
@@ -13,15 +13,6 @@ require("./routes")(app);
 
 db.sequelize.sync();
 
-// db.sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log("Connection has been established successfully.");
-//   })
-//   .catch((err) => {
-//     console.error("Unable to connect to the database:", err);
-//   });
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`app listening at http://localhost:${port}`);
 });
