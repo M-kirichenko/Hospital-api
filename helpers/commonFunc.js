29dfb909f0ff84b4b9a1a7b0e899a0a7bdb9fd58
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-05-06 14:02:48 +0400

    final-final-final fix

--- helpers/commonFunc.js
+++ helpers/commonFunc.js
@@ -1,6 +1,6 @@
 const genToken = (tokenParams) => {
   const jwt = require("jsonwebtoken");
-  const token = jwt.sign({ ...tokenParams }, process.env.TOKEN_KEY, {
+  const token = jwt.sign(tokenParams, process.env.TOKEN_KEY, {
     expiresIn: "2h",
   });
   return token;
