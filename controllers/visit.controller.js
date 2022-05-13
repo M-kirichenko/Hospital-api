94b02774e7fd7e560aaf17e9ea973d41903c603a
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-05-13 17:05:02 +0400

    code-refactor

--- controllers/visit.controller.js
+++ controllers/visit.controller.js
@@ -1,6 +1,6 @@
-const { Visit } = require("../models");
-const { validName } = require("../helpers/validator");
 const moment = require("moment");
+const { Visit } = require("../models");
+const { validateVisitBody } = require("../helpers/commonFunc");
 
 exports.createVisit = async (req, res) => {
   const { body } = req;
@@ -9,18 +9,7 @@ exports.createVisit = async (req, res) => {
   if (!text || !date || !doctor_id || !patient_name)
     return res.status(422).send({ msg: "All visit info is required" });
 
-  const errors = [];
-
-  if (!text.length) {
-    errors.push("Visit text can't be empty");
-  }
-
-  if (!validName(patient_name))
-    errors.push(
-      "Patient name must be composed only of letters and be at least 3 letters long"
-    );
-
-  if (!moment(date, "DD.MM.YYYY").isValid()) errors.push("Invalid date");
+  const errors = validateVisitBody(body);
 
   if (errors.length) return res.status(422).send({ msg: errors });
 
@@ -62,10 +51,11 @@ exports.updateOne = async (req, res) => {
   if (!text && !date && !doctor_id && !patient_name)
     return res.status(422).send({ msg: "Nothing to update" });
 
-  try {
-    if (!moment(date, "DD.MM.YYYY").isValid())
-      return res.status(422).send({ msg: "Invalid Date" });
+  const errors = validateVisitBody(body);
 
+  if (errors.length) return res.status(422).send({ msg: errors });
+
+  try {
     body.date = moment.utc(date, "DD.MM.YYYY");
 
     const updated = await Visit.update(body, {
